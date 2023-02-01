75d787107b08f4a193639dd962cbf1f6c0a08c5f
WardoCandelas <osskar.alc@gmail.com>
2023-02-01 14:18:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-//Validamos si el usuario esta logeado, para que no pueda acceder a el index desde el URL
+//Validamos si el usuario esta logeado, para que no pueda acceder a el index
 if(empty($_SESSION['id_usuario'])){
     header("location: login");
 }
