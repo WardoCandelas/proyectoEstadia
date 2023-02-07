e2c7088b0b38d1a61f83404500e7715245287c85
WardoCandelas <osskar.alc@gmail.com>
2023-02-07 14:01:27 -0600

    Update nuevoExpedienteAction.php

--- expediente/nuevoExpedienteAction.php
+++ expediente/nuevoExpedienteAction.php
@@ -1,14 +1,14 @@
 <?php 
 include "../conexionBD/conexionBD.php";
 
-
+date_default_timezone_set('America/Mexico_City');
 //Recibimos las datos del formulario
 $nombreExpediente = $_POST['no_e'];
 $claveExpediente = $_POST['c_e'];
 $numeroExpediente = $_POST['nu_e'];
 $yearExpediente = $_POST['y_e'];
 $observacionesExpediente = $_POST['o_e'];
-
+$fechaActual = date('Y-m-d');
 //Asignamos la ruta 
 $micarpeta = "../archivos/".$nombreExpediente."-".$claveExpediente."-".$numeroExpediente."-".$yearExpediente;
 
@@ -17,9 +17,9 @@ if (!file_exists($micarpeta)) {
     //Creamos carpeta
     mkdir($micarpeta, 0777, true);
     //Insertamos los datos en la BD
-    $insertExpediente = "INSERT INTO expedientes VALUES (null, '$nombreExpediente', '$claveExpediente', '$numeroExpediente', '$yearExpediente', '$observacionesExpediente', '0')";
+    $insertExpediente = "INSERT INTO expedientes VALUES (null, '$nombreExpediente', '$claveExpediente', '$numeroExpediente', '$yearExpediente', '$observacionesExpediente', '0', '$fechaActual')";
     mysqli_query($con, $insertExpediente); 
-    //echo $insertExpediente;?>
+    echo $insertExpediente;?>
 <br>
 <div class="alert alert-success" role="alert">
   Expediente Creado Correctamente!
